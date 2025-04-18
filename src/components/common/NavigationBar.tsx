2933f20a0a3822354276765a29fa3bf1bf353099
mnmhbbb <mnmhb64@gmail.com>
2025-04-18 18:25:10 +0900

    feat: 상단 네비바 컴포넌트 생성

---
+++ src/components/common/NavigationBar.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const NavigationBar = () => {
+  return (
+    <nav className="flex items-center justify-between mb-4">
+      <Link href="/">
+        <h1 className="text-2xl font-bold">KNOCK KNOCK | 면담 예약 사이트</h1>
+      </Link>
+      <div className="flex gap-4 items-center">
+        <Link href="/my" className="text-sm">
+          👤 내 정보
+        </Link>
+        <Button variant="outline" size="sm">
+          로그아웃
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
