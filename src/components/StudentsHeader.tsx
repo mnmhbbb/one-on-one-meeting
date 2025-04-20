09bbfac23cfbc7a70be57fe328a2902af068fc8f
mnmhbbb <mnmhb64@gmail.com>
2025-04-21 00:57:44 +0900

    feat: 학생용 페이지 헤더 컴포넌트 추가

---
+++ src/components/StudentsHeader.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { memo } from "react";
+import { Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const StudentsHeader = () => {
+  const pathname = usePathname();
+
+  const links = [
+    { href: "/my", label: "MY" },
+    { href: "/interview-requests", label: "신청현황" },
+  ];
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-2 items-end">
+        <div className="flex gap-2 h-fit">
+          {links.map((link) => (
+            <Link
+              key={link.href}
+              className={`border py-1.5 px-8 rounded-md flex items-center rounded-b-none border-b-0 ${
+                pathname === link.href ? "bg-stone-600 text-white" : ""
+              }`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </div>
+        {/* TODO: 학생 이름 표시 */}
+        <h2 className="text-xl text-center font-semibold mb-4">OOO님의 면담 일정</h2>
+        {/* TODO: 교수님 검색 모달 */}
+        <Button variant="outline" className="text-neutral-400 max-w-auto mb-4 max-w-fit ml-auto">
+          <Search />
+          교수님을 검색하세요.
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default memo(StudentsHeader);
