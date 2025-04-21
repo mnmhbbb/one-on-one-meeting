56aa0fc200078a191706f01d0eef1ef94d8a8be9
mnmhbbb <mnmhb64@gmail.com>
2025-04-22 00:49:56 +0900

    feat: 면담 상태 뱃지 컴포넌트 추가

---
+++ src/components/common/StatusBadge.tsx
@@ -0,0 +1,23 @@
+import { InterviewStatus, STATUS_COLORS, STATUS_LABELS } from "@/common/const";
+import { cn } from "@/lib/utils";
+
+interface StatusBadgeProps {
+  status: InterviewStatus;
+  className?: string;
+}
+
+const StatusBadge = ({ status, className }: StatusBadgeProps) => {
+  return (
+    <div
+      className={cn(
+        "inline-flex items-center justify-center px-2 py-1 rounded-md text-xs w-full",
+        STATUS_COLORS[status],
+        className,
+      )}
+    >
+      {STATUS_LABELS[status]}
+    </div>
+  );
+};
+
+export default StatusBadge;
