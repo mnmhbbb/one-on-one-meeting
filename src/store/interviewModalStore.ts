6c851859b2b3e5ed8a76f37c7d08fd83c975a00f
mnmhbbb <mnmhb64@gmail.com>
2025-05-05 21:00:38 +0900

    refactor: zustand 미들웨어(immer, devtools 적용) 추가 및 기존 스토어에 적용

--- src/store/interviewModalStore.ts
+++ src/store/interviewModalStore.ts
@@ -1,25 +1,38 @@
-import { create } from "zustand";
-
 import { InterviewModalType } from "@/common/const";
+import { createStore } from "@/store/store";
+import { InterviewInfo } from "@/utils/data/mockData";
 
 type InterviewModalState = {
   isOpen: boolean;
-  isProfessorSearchOpen: boolean;
+  isProfessorSearchOpen: boolean; // 교수 검색 모달 열림 여부
   interviewId: string | null;
   type: InterviewModalType | null;
+  interviewInfo: InterviewInfo | null;
+
   open: (id: string, type: InterviewModalType) => void;
   close: () => void;
   openProfessorSearch: () => void;
   closeProfessorSearch: () => void;
+  setInterviewInfo: (info: InterviewInfo) => void;
+  resetInterviewModal: () => void;
 };
 
-export const useInterviewModalStore = create<InterviewModalState>(set => ({
+export const useInterviewModalStore = createStore<InterviewModalState>(set => ({
   isOpen: false,
   isProfessorSearchOpen: false,
   interviewId: null,
   type: null,
+  interviewInfo: null,
   open: (id, type) => set({ isOpen: true, interviewId: id, type }),
   close: () => set({ isOpen: false, interviewId: null, type: null }),
   openProfessorSearch: () => set({ isProfessorSearchOpen: true }),
   closeProfessorSearch: () => set({ isProfessorSearchOpen: false }),
+  setInterviewInfo: info => set({ interviewInfo: info }),
+  resetInterviewModal: () =>
+    set({
+      isOpen: false,
+      type: null,
+      interviewId: null,
+      interviewInfo: null,
+    }),
 }));
