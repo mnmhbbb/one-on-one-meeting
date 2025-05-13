347383c9ee296532f7963b1480eb142674face46
mnmhbbb <mnmhb64@gmail.com>
2025-05-13 23:07:15 +0900

    feat: 로그인 redirectTo 기본값 추가

--- src/components/auth/RedirectInput.tsx
+++ src/components/auth/RedirectInput.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 /**
  * 로그인 완료 후 이동할 대상 경로(redirectTo)를 설정하는 입력 필드
+ * redirect 기본값은 각 유저의 /my
  */
 const RedirectInput = () => {
   const searchParams = useSearchParams();
-  const redirectTo = searchParams.get("redirectTo") ?? "/";
+  const pathname = usePathname();
+  const defaultRedirectTo = pathname.startsWith("/student") ? "/student/my" : "/professor/my";
+  const redirectTo = searchParams.get("redirectTo") ?? defaultRedirectTo;
 
   return <input type="hidden" name="redirectTo" value={redirectTo} />;
 };
