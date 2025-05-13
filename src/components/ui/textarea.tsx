591bdba7f46c37652b956749df44773147ab452e
mnmhbbb <mnmhb64@gmail.com>
2025-05-13 21:34:18 +0900

    style: textarea 기본 스타일에 resize-none 추가

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -30,7 +30,7 @@ function Textarea({ className, maxLength, value, defaultValue, ...props }: Texta
         ref={textareaRef}
         data-slot="textarea"
         className={cn(
-          "border-input focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 !w-full rounded-md border bg-transparent px-3 pt-2 pb-6 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "border-input focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 !w-full resize-none rounded-md border bg-transparent px-3 pt-2 pb-6 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className
         )}
         maxLength={maxLength}
